d466ccb820a50a5a86edb861b577ba99e4f81c43
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-06-28 20:09:54 +0300

    fix(tenant): server/routes/reportRoutes.js

--- server/routes/reportRoutes.js
+++ server/routes/reportRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/auth');
-const {
-  getDashboard, getInventoryReport, getPurchaseReport
-} = require('../controllers/reportController');
+const { tenantGuard } = require('../middleware/tenant');
+const { getDashboard, getInventoryReport, getPurchaseReport } = require('../controllers/reportController');
 
-router.get('/dashboard', protect, getDashboard);
-router.get('/inventory', protect, getInventoryReport);
-router.get('/purchases', protect, getPurchaseReport);
+router.get('/dashboard', protect, tenantGuard, getDashboard);
+router.get('/inventory', protect, tenantGuard, getInventoryReport);
+router.get('/purchases', protect, tenantGuard, getPurchaseReport);
 
 module.exports = router;
