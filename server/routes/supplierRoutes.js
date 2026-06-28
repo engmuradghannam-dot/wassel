4ed62a8b7b53fb32c9d91a1a7d8f7ef2ed00f464
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-06-28 21:45:41 +0300

    fix: remove tenantGuard/checkPlanLimit from supplierRoutes.js

--- server/routes/supplierRoutes.js
+++ server/routes/supplierRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
-const { tenantGuard } = require('../middleware/tenant');
+const {  } = require('../middleware/tenant');
 const { getSuppliers, getSupplier, createSupplier, updateSupplier, deleteSupplier } = require('../controllers/supplierController');
 
 router.route('/')
-  .get( protect, tenantGuard, getSuppliers)
-  .post(protect, tenantGuard, authorize('admin','manager'), createSupplier);
+  .get( protect, getSuppliers)
+  .post(protect, authorize('admin','manager'), createSupplier);
 
 router.route('/:id')
-  .get(   protect, tenantGuard, getSupplier)
-  .put(   protect, tenantGuard, authorize('admin','manager'), updateSupplier)
-  .delete(protect, tenantGuard, authorize('admin'), deleteSupplier);
+  .get(   protect, getSupplier)
+  .put(   protect, authorize('admin','manager'), updateSupplier)
+  .delete(protect, authorize('admin'), deleteSupplier);
 
 module.exports = router;
