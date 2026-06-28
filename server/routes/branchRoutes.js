03b051c1df301739665fbeb92d00a6181438a5d6
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-06-28 21:45:36 +0300

    fix: remove tenantGuard/checkPlanLimit from branchRoutes.js

--- server/routes/branchRoutes.js
+++ server/routes/branchRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
-const { tenantGuard, checkPlanLimit } = require('../middleware/tenant');
+const { checkPlanLimit } = require('../middleware/tenant');
 const { getBranches, getBranch, createBranch, updateBranch, deleteBranch } = require('../controllers/branchController');
 
 router.route('/')
-  .get( protect, tenantGuard, getBranches)
-  .post(protect, tenantGuard, authorize('admin','manager'), checkPlanLimit('branches'), createBranch);
+  .get( protect, getBranches)
+  .post(protect, authorize('admin','manager'), createBranch);
 
 router.route('/:id')
-  .get(   protect, tenantGuard, getBranch)
-  .put(   protect, tenantGuard, authorize('admin','manager'), updateBranch)
-  .delete(protect, tenantGuard, authorize('admin'), deleteBranch);
+  .get(   protect, getBranch)
+  .put(   protect, authorize('admin','manager'), updateBranch)
+  .delete(protect, authorize('admin'), deleteBranch);
 
 module.exports = router;
