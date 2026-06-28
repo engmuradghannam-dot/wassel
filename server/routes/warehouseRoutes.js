6b0cbf5c67fb74ad099682c45c703af271f39a22
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-06-28 20:09:30 +0300

    fix(tenant): add tenantGuard to server/routes/warehouseRoutes.js

--- server/routes/warehouseRoutes.js
+++ server/routes/warehouseRoutes.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../middleware/auth');
-const {
-  getWarehouses, getWarehouse, createWarehouse, updateWarehouse, deleteWarehouse
-} = require('../controllers/warehouseController');
+const { tenantGuard } = require('../middleware/tenant');
+const { getWarehouses, getWarehouse, createWarehouse, updateWarehouse, deleteWarehouse } = require('../controllers/warehouseController');
 
 router.route('/')
-  .get(protect, getWarehouses)
-  .post(protect, authorize('admin', 'manager'), createWarehouse);
+  .get( protect, tenantGuard, getWarehouses)
+  .post(protect, tenantGuard, authorize('admin','manager'), createWarehouse);
 
 router.route('/:id')
-  .get(protect, getWarehouse)
-  .put(protect, authorize('admin', 'manager'), updateWarehouse)
-  .delete(protect, authorize('admin'), deleteWarehouse);
+  .get(   protect, tenantGuard, getWarehouse)
+  .put(   protect, tenantGuard, authorize('admin','manager'), updateWarehouse)
+  .delete(protect, tenantGuard, authorize('admin'), deleteWarehouse);
 
 module.exports = router;
